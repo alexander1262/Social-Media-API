5b37a24ca70f61bac91419844f1e3ae2c82969e8
Sander Weiss <weisssander@gmail.com>
2023-04-10 17:05:48 -0400

    models started

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, Types, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'thought',
+        },
+        friends: {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        }
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    })
+
+const User = model('user', userSchema);
+
+module.exports = User;
